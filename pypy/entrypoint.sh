9cee8331f392ad62b2cd1c1dac7044bf21f6641e
MrAvox <143508913+avoxmr@users.noreply.github.com>
2023-08-31 18:38:55 +0100

    Update entrypoint.sh

--- pypy/entrypoint.sh
+++ pypy/entrypoint.sh
@@ -9,5 +9,8 @@ export INTERNAL_IP
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
+# Print Pypy Version
+pypy --version
+
 # Run the Server
 eval ${MODIFIED_STARTUP}
